2ba83e88a526655a3d1e0e7a2f46dfea8eb73810
Nataliia <maksimeyko@ukr.net>
2023-02-27 19:39:29 +0200

    update files

--- react-native-project/Screens/RegistrationScreen/RegistrationScreen.js
+++ react-native-project/Screens/RegistrationScreen/RegistrationScreen.js
@@ -5,39 +5,52 @@ import {
   View,
   TextInput,
   ImageBackground,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform
 } from "react-native";
 
 export default function RegistrationScreen() {
   const [value, setValue] = useState("");
 
   const inputHandler = (text) => setValue(text);
+
   return (
     <View style={styles.container}>
       <ImageBackground
         style={styles.image}
         source={require("../../assets/image/registrationScreenPhoto.jpg")}
       >
-        <Text style={styles.text}>Регистрация</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Логин"
-          value={value}
-          onChangeText={inputHandler}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Адрес электронной почты"
-          value={value}
-          onChangeText={inputHandler}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Пароль Показать"
-          value={value}
-                  onChangeText={inputHandler}
-                  secureTextEntry={true}
-              />
-              <Button title='Зарегистрироваться'></Button>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <View style={styles.wrapper}>
+          <Text style={styles.text}>Регистрация</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Логин"
+            value={value}
+            onChangeText={inputHandler}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Адрес электронной почты"
+            value={value}
+            onChangeText={inputHandler}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Пароль Показать"
+            value={value}
+            onChangeText={inputHandler}
+            secureTextEntry={true}
+          />
+          <TouchableOpacity
+            style={style.btn}
+            title="Зарегистрироваться"
+          ></TouchableOpacity>
+          <Text>Уже есть аккаунт? Войти</Text></View>
+        </KeyboardAvoidingView>{" "}
       </ImageBackground>
     </View>
   );
@@ -56,6 +69,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  wrapper:{
+    backgroundColor: "#ffffff",
+    marginHorizontal: 20,
+    padding: 16,
+  },
   text: {
     fontFamily: "Roboto",
     fontStyle: normal,
@@ -70,11 +88,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#E8E8E8",
     background: linear - gradient("0deg, #F6F6F6, #F6F6F6"),
-    // boxSizing: border - box,
-    // position: absolute,
-    // left: 0,
-    // right: 0,
-    // top: 0,
-    // bottom: 0,
+  },
+  btn: {
+    display: 1,
+    flexDirection: column,
+    alignItems: center,
+    padding: 32,
+    gap: 12,
+    height: 51,
+    background: "#FF6C00",
+    borderRadius: 10,
   },
 });
