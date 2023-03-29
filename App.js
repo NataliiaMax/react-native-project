fd4cb48d936b3871376e04ffd87a8f69707274b9
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2023-03-29 21:06:49 +0300

    update app

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+
+// SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <TouchableWithoutFeedback>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <RegistrationScreen />
+        <LoginScreen />
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
